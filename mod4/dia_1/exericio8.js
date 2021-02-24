6bb64e7b8859e6841ecc713a709ef64266eae556
Viegas <mvrdgs@gmail.com>
2021-02-24 20:02:36 -0300

    exercicio verificaçao de numero par entre 3 numeros

---
+++ mod4/dia_1/exericio8.js
@@ -0,0 +1,9 @@
+let numero1 = 7;
+let numero2 = 5;
+let numero3 = 3;
+
+if (numero1 % 2 == 0 || numero2 % 2 == 0 || numero3 % 2 == 0) {
+  console.log("Ao menos um dos numeros é par");
+} else {
+  console.log("Nenhum dos numeros é par");
+}
