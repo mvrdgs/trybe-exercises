d967a5f14eeb5edd49570d4f325cd9e45a7a2fd6
Viegas <mvrdgs@gmail.com>
2021-02-25 15:42:33 -0300

    resolvido exercicio 4

--- mod4/dia_2/exercicio3.js
+++
@@ -1,8 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let somaNumbers = 0;
-
-for (let i = 0; i < numbers.length; i++) {
-  somaNumbers += numbers[i];
-}
-
-console.log(somaNumbers/numbers.length);
