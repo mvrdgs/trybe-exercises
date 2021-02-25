e84ea1a87983ea79942f8b5a04ebd1455d94dfb3
Viegas <mvrdgs@gmail.com>
2021-02-25 17:21:56 -0300

    agora sim, ta resolvido mesmo

--- mod4/dia_2/exercicioBonus3.js
+++ mod4/dia_2/exercicioBonus3.js
@@ -1,9 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbersMultiplied = [];
 
-for (let i = 1; i < numbers.length; i++){
-  var multiplation = numbers[i] * numbers[i-1];
-  numbersMultiplied.push(multiplation);
+for (let i = 0; i < numbers.length; i++){
+  if(i+1 < numbers.length){
+    let multiplation = numbers[i] * numbers[i+1];
+    numbersMultiplied.push(multiplation);
+  } else {
+    let multiplication = 2 * numbers[i];
+    numbersMultiplied.push(multiplication);
+  }
 }
 
 for (let i = 0; i < numbersMultiplied.length; i++){
