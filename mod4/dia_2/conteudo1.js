3f41b1b453417ce489d57c0c7e7e39c63a61675e
Viegas <mvrdgs@gmail.com>
2021-02-25 15:14:33 -0300

    feito exercicio 2 conteudo 1

--- mod4/dia_2/conteudo1.js
+++ mod4/dia_2/conteudo1.js
@@ -2,3 +2,7 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu.indexOf('Serviços');
 
 console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
