7e791c7a0373530573e43f1080c023767aa46156
Viegas <mvrdgs@gmail.com>
2021-02-25 15:30:53 -0300

    correçoes

--- mod4/dia_2/conteudo2.js
+++ mod4/dia_2/conteudo2.js
@@ -1,5 +1,5 @@
 let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
 
-for (i = 0; i < groceryList.length; i++) {
+for (let i = 0; i < groceryList.length; i++) {
   console.log(groceryList[i]);
 }
