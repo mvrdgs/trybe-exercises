424f7095fce373ffbfd20476c2021e4240225a74
Viegas <mvrdgs@gmail.com>
2021-02-25 17:10:24 -0300

    alterado atribuiçao para push

--- mod4/dia_2/exercicioBonus3.js
+++ mod4/dia_2/exercicioBonus3.js
@@ -3,7 +3,7 @@ let numbersMultiplied = [];
 
 for (let i = 1; i < numbers.length; i++){
   var multiplation = numbers[i] * numbers[i-1];
-  numbersMultiplied[i-1] = multiplation;
+  numbersMultiplied.push(multiplation);
 }
 
 for (let i = 0; i < numbersMultiplied.length; i++){
