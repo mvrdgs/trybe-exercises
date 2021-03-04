c316374e0ebfed5eb735c108640b121004b30a27
Viegas <mvrdgs@gmail.com>
2021-03-04 15:13:36 -0300

    Feito conteudo 1

---
+++ mod5/dia_2/conteudo1.js
@@ -0,0 +1,23 @@
+// Acesse o elemento elementoOndeVoceEsta .
+let whereYouAre = document.getElementById("elementoOndeVoceEsta");
+
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+whereYouAre.parentNode.style.color = 'blue';
+
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+whereYouAre.firstElementChild.innerText = 'bla bla bla'
+
+// Acesse o primeiroFilho a partir de pai .
+whereYouAre.parentNode.firstElementChild;
+
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+whereYouAre.previousElementSibling;
+
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+whereYouAre.nextSibling;
+
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+whereYouAre.nextElementSibling;
+
+// Agora acesse o terceiroFilho a partir de pai .
+whereYouAre.parentNode.lastElementChild.previousElementSibling;
