6db997f1bbe2ef42ce532c682a956991949cb1c7
Viegas <mvrdgs@gmail.com>
2021-03-05 22:06:58 -0300

    adicionado botao feriados e funçao mudança de cor

--- mod5/dia_3/exercicio.js
+++ mod5/dia_3/exercicio.js
@@ -20,24 +20,69 @@ createDaysOfTheWeek();
 // Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
 // Copiar
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-const holidays = [24, 25, 31]
+const fridays = [4, 11, 18, 25];
 
-let daysList = document.getElementById('days');
+function calendarDays(array) {
+  let daysList = document.getElementById('days');
 
-for(index = 0; index < dezDaysList.length; index += 1) {
-  let dayNumber;
-  dayNumber = document.createElement('li');
-  dayNumber.className = 'day';
-  dayNumber.innerText = dezDaysList[index];
- 
-  if(parseInt(dayNumber.innerText) === 24 || parseInt(dayNumber.innerText) === 25 || parseInt(dayNumber.innerText) === 31) {
-    dayNumber.className += ' holiday';
+  for(index = 0; index < array.length; index += 1) {
+    let dayNumber;
+    dayNumber = document.createElement('li');
+    dayNumber.className = 'day';
+    dayNumber.innerText = array[index];
+  
+    if(parseInt(dayNumber.innerText) === 24 || parseInt(dayNumber.innerText) === 25 || parseInt(dayNumber.innerText) === 31) {
+      dayNumber.className += ' holiday';
+    }
+
+    if(parseInt(dayNumber.innerText) === 4 || parseInt(dayNumber.innerText) === 11 || parseInt(dayNumber.innerText) === 18 || parseInt(dayNumber.innerText) === 25) {
+      dayNumber.className += ' friday';
+    }
+    daysList.appendChild(dayNumber);
   }
-
-  if(parseInt(dayNumber.innerText) === 4 || parseInt(dayNumber.innerText) === 11 || parseInt(dayNumber.innerText) === 18 || parseInt(dayNumber.innerText) === 25) {
-    dayNumber.className += ' friday';
-  }
-  daysList.appendChild(dayNumber);
 }
 
+calendarDays(dezDaysList);
+// Exercício 2:
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+// Adicione a este botão a ID "btn-holiday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+function createHolidayButton(buttonName){
+  let buttonsContainer = document.querySelector('.buttons-container');
+  let holidayButton = document.createElement('button');
+  holidayButton.id = 'btn-holiday';
+  holidayButton.innerText = buttonName;
+  buttonsContainer.appendChild(holidayButton);
+}
+
+createHolidayButton('Feriados');
+
+// Exercício 3:
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+let holidayButton = document.querySelector('button');
+
+holidayButton.addEventListener("click", clickColor);
+
+let active = 0;
+
+function clickColor() {
+  let holidays = document.getElementsByClassName('holiday');
+  if(active === 0){
+    for(let index = 0; index < holidays.length; index += 1){
+      holidays[index].style.backgroundColor = 'rgb(0,0,255)';
+    }
+    active = 1;
+  } else {
+    for(let index = 0; index < holidays.length; index += 1){
+      holidays[index].style.backgroundColor = 'rgb(238,238,238)';
+    }
+    active = 0;
+  }
+};
+
+// Exercício 4:
+// Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+// Adicione a este botão o ID "btn-friday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
 
