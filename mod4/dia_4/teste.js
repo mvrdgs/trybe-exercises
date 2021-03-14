6f9af27c70d5ae190bd771bfe146adb141727635
Viegas <mvrdgs@gmail.com>
2021-03-14 12:46:13 -0300

    corrigido exercicio numeros romanos, agora funciona plenamente

--- mod4/dia_4/teste.js
+++
@@ -1,5 +0,0 @@
-let numeros = [1, 2, 3, 4, 5, 6];
-
-for (let index in numeros) {
-  console.log(+index + 1);
-}
--- mod5/dia_4/script.js
+++ mod5/dia_4/script.js
@@ -1,21 +1,25 @@
-localStorage.setItem("bgColor", "blue");
-localStorage.setItem("textColor", "red");
-localStorage.setItem('fontSize', '1.5em');
-localStorage.setItem('lineHeight', '30px');
-localStorage.setItem('fontFamily', 'Arial');
+if(typeof(Storage) != "undefined") {
+  localStorage.setItem("bgColor", "blue");
+  localStorage.setItem("textColor", "red");
+  localStorage.setItem('fontSize', '1.5em');
+  localStorage.setItem('lineHeight', '30px');
+  localStorage.setItem('fontFamily', 'Arial');
 
-function setPreferences() {
-  let text = document.querySelector('.text');
-  let bgColor = localStorage.getItem('bgColor');
-  let paragraphs = document.querySelectorAll('p');
+  function setPreferences() {
+    let text = document.querySelector('.text');
+    let bgColor = localStorage.getItem('bgColor');
+    let paragraphs = document.querySelectorAll('p');
 
-  for(let index = 0; index < paragraphs.length; index += 1) {
-    paragraphs[index].style.backgroundColor = bgColor;
+    for(let index = 0; index < paragraphs.length; index += 1) {
+      paragraphs[index].style.backgroundColor = bgColor;
+    }
+    text.style.color = localStorage.getItem('textColor');
+    text.style.fontSize = localStorage.getItem('fontSize');
+    text.style.lineHeight = localStorage.getItem('lineHeight');
+    text.style.fontFamily = localStorage.getItem('fontFamily');
   }
-  text.style.color = localStorage.getItem('textColor');
-  text.style.fontSize = localStorage.getItem('fontSize');
-  text.style.lineHeight = localStorage.getItem('lineHeight');
-  text.style.fontFamily = localStorage.getItem('fontFamily');
-}
 
-setPreferences();
+  setPreferences();
+
+  localStorage.clear();
+}
