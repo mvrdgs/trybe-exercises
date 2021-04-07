6bcc588c6108e6301d184bcb8eb95f6a7147a1ff
Viegas <mvrdgs@gmail.com>
2021-04-07 15:46:09 -0300

    conteudo 2 - desestruturaçao

--- mod8/dia_5/conteudo1.js
+++ mod8/dia_5/conteudo1.js
@@ -1,7 +1,5 @@
-// Faça uma lista com as suas frutas favoritas
 const specialFruit = ['kiwi', 'banana', 'abacaxi'];
 
-// Faça uma lista de complementos que você gostaria de adicionar
 const additionalItens = ['laranja', 'pêssego', 'melão'];
 
 const fruitSalad = (fruit, additional) => {
