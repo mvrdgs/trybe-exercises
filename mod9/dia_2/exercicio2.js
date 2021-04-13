b314b28a3ae558b4b5912ebdead3cd373c2876f2
Viegas <mvrdgs@gmail.com>
2021-04-13 20:04:22 -0300

    exercicio 2 a 5

--- mod9/dia_2/exercicio2.js
+++ mod9/dia_2/exercicio2.js
@@ -1,16 +1,21 @@
-const promise = new Promise((resolve, reject) => {
-  const tenRandomNumbers = [];
-  for (let index = 0; index < 10; index += 1) {
-    tenRandomNumbers.push(Math.ceil(Math.random() * 50));
-  }
-  tenRandomNumbers.sort((a, b) => a - b);
-  const numbersToSquare = [];
-  tenRandomNumbers.forEach((number) => numbersToSquare.push(number * number));
-  const total = numbersToSquare.reduce((result, current) => result += current);
-  console.log(total)
-  
-  if (total < 8000) return reject('Promise Rejeitada');
-  resolve('Promise resolvida');
-})
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+const fetchPromise = () => {
+  const promise = new Promise((resolve, reject) => {
+
+    const tenRandomNumbers = Array.from(
+      { length: 10 },
+      () => Math.ceil(Math.random() * 50),
+    )
+
+    const total = tenRandomNumbers.map((number) => number * number)
+      .reduce((result, current) => result += current);
+
+    total > 8000 ? reject('É mais de 8 mil! Essa promise deve estar quebrada') : resolve(total);
+  });
+
+  promise
+    .then((result) => [2, 3, 5, 10].map((number) => result / number))
+    .then((result) => console.log(result.reduce((result, current) => result += current)))
+    .catch((error) => console.log(error));
+}
+
+fetchPromise();
