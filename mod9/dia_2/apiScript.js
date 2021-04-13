4d689ae73947bd47a74c23720217a655ed972fcb
Viegas <mvrdgs@gmail.com>
2021-04-13 18:34:51 -0300

    exercicio 1, criando piada aleatoria

---
+++ mod9/dia_2/apiScript.js
@@ -0,0 +1,17 @@
+// apiScript.js     
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+  
+  const jokeContainer = document.querySelector('#jokeContainer');
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => jokeContainer.innerHTML = data.joke);
+};
+
+window.onload = () => fetchJoke();
