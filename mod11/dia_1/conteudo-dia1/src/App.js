98bca97e3606ee867379212f44923c7cb5b11d8a
Viegas <mvrdgs@gmail.com>
2021-04-26 19:03:19 -0300

    conteudo

---
+++ mod11/dia_1/conteudo-dia1/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Header from './Component';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ mod11/dia_1/conteudo-dia1/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Maurício</h1>
+        <p>Tamo aí estudando da melhor maneira possível</p>
+      </header>
+    );
+  };
+} ;
+
+export default Header;
