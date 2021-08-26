26ff72f11c5583d3b29a0b06c6c16eea7fbf6809
Viegas <mvrdgs@gmail.com>
2021-08-26 15:16:51 -0300

    adicionado verificaçao de tipo a funçao

--- mod26/dia3/index.js
+++ mod26/dia3/index.js
@@ -1,4 +1,5 @@
 const testaNumero = (numero) => {
+  if (typeof numero !== 'number') return 'Insira um valor numérico';
   if (numero > 0) return 'positivo';
   if (numero === 0) return 'neutro';
   if (numero < 0) return 'negativo';
