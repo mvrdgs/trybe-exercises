eaec571acbb6e45e68c6ae126a225fc81157b5be
Viegas <mvrdgs@gmail.com>
2021-09-02 18:25:41 -0300

    refatorado com mongodb

--- mod27/conteudo/models/Author.js
+++ mod27/conteudo/models/Author.js
@@ -1,4 +1,5 @@
 const connection = require('./connection');
+const { ObjectId } = require('mongodb');
 
 const getNewAuthor = ({ id, firstName, middleName, lastName }) => {
   const fullName = [firstName, middleName, lastName].filter((name) => name)
@@ -22,21 +23,51 @@ const serialize = (authorData) => {
   };
 };
 
-const getAll = async () => {
-  const [authors] = await connection.execute('SELECT id, first_name, middle_name, last_name FROM authors');
+// const getAll = async () => {
+//   const [authors] = await connection.execute('SELECT id, first_name, middle_name, last_name FROM authors');
 
-  return authors.map(serialize).map(getNewAuthor);
+//   return authors.map(serialize).map(getNewAuthor);
+// };
+
+const getAll = async () => {
+  return connection()
+    .then((db) => db.collection('authors').find().toArray())
+    .then((authors) => {
+      return authors.map(({ _id, firstName, middleName, lastName }) => {
+        return getNewAuthor({
+          id: _id,
+          firstName,
+          middleName,
+          lastName,
+        });
+      });
+    });
 };
 
+// const getByAuthorId = async (id) => {
+//   const [authorData] = await connection.execute(
+//     'SELECT id, first_name, middle_name, last_name FROM authors WHERE id=?',
+//     [id],
+//   );
+
+//   if (!authorData.length) return null;
+
+//   const { firstName, middleName, lastName } = authorData.map(serialize)[0];
+
+//   return getNewAuthor({
+//     id,
+//     firstName,
+//     middleName,
+//     lastName,
+//   });
+// };
+
 const getByAuthorId = async (id) => {
-  const [authorData] = await connection.execute(
-    'SELECT id, first_name, middle_name, last_name FROM authors WHERE id=?',
-    [id],
-  );
+  const authorData = await connection().then((db) => db.collection('authors').findOne(ObjectId(id)));
 
-  if (!authorData.length) return null;
+  if (!authorData) return null;
 
-  const { firstName, middleName, lastName } = authorData.map(serialize)[0];
+  const { firstName, middleName, lastName } = authorData;
 
   return getNewAuthor({
     id,
@@ -53,10 +84,15 @@ const isValid = (firstName, middleName, lastName) => {
   return true;
 };
 
-const create = async (firstName, middleName, lastName) => connection.execute(
-  'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
-  [firstName, middleName, lastName],
-);
+// const create = async (firstName, middleName, lastName) => connection.execute(
+//   'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
+//   [firstName, middleName, lastName],
+// );
+
+const create = async (firstName, middleName, lastName) => {
+  await connection()
+    .then((db) => db.collection('authors').insertOne({ firstName, middleName, lastName }));
+}
 
 module.exports = {
   getAll,
--- mod27/conteudo/models/Books.js
+++ mod27/conteudo/models/Books.js
@@ -1,36 +1,56 @@
 const connection = require('./connection');
+const { ObjectId } = require('mongodb');
+
+// const getAll = async () => {
+//   const [books] = await connection.execute('SELECT id, title, author_id FROM books');
+
+//   return books;
+// };
 
 const getAll = async () => {
-  const [books] = await connection.execute('SELECT id, title, author_id FROM books');
+  return connection()
+    .then((db) => db.collection('books').find().toArray());
+}
 
-  return books;
-};
+// const getByBookId = async (id) => {
+//   const [books] = await connection.execute('SELECT id, title, author_id FROM books WHERE id=?', [id]);
+
+//   return books.length ? books : null;
+// };
 
 const getByBookId = async (id) => {
-  const [books] = await connection.execute('SELECT id, title, author_id FROM books WHERE id=?', [id]);
+  const books = await connection()
+    .then((db) => db.collection('books').findOne(ObjectId(id)));
 
-  return books.length ? books : null;
+  return books ? books : null;
 };
 
 const isValid = async (title, authorId) => {
   if (!title || title.length < 3) return false;
   if (!authorId) return false;
 
-  const [authors] = await await connection.execute(
-    'SELECT id FROM authors WHERE id=?',
-    [authorId],
-  );
+  // const [authors] = await connection.execute(
+  //   'SELECT id FROM authors WHERE id=?',
+  //   [authorId],
+  // );
+
+  const authors = await connection()
+    .then((db) => db.collection('authors').findOne(ObjectId(authorId)));
   
-  if (!authors.length) return false;
-  console.log('testando length', !authors.length);
+  if (!authors) return false;
 
   return true;
 };
 
-const create = async (title, authorId) => connection.execute(
-  'INSERT INTO model_example.books (title, author_id) VALUES(?, ?)',
-  [title, authorId], 
-);
+// const create = async (title, authorId) => connection.execute(
+//   'INSERT INTO model_example.books (title, author_id) VALUES(?, ?)',
+//   [title, authorId], 
+// );
+
+const create = async (title, authorId) => {
+  await connection()
+    .then((db) => db.collection('books').insertOne({ title, authorId }));
+};
 
 module.exports = {
   getAll,
