147a6adb24f4c70f9912be042e1a40ed2175f153
Viegas <mvrdgs@gmail.com>
2021-11-30 12:14:10 -0300

    funçao media e funçao imprime quadrado de asteriscos

--- mod32/dia_1/exercicios.py
+++ mod32/dia_1/exercicios.py
@@ -4,3 +4,19 @@ def highest_number(n1, n2):
 
 highest_number(10, 5)
 highest_number(5, 10)
+
+
+def media(numbers):
+    media = sum(numbers) / len(numbers)
+    print(media)
+
+
+media([1, 2, 3, 4, 5])
+
+
+def asterisks_square(n):
+    for index in range(n):
+        print("*" * n)
+
+
+asterisks_square(10)
