c788a7f187ff7479057ba854391cc8ac3ee04967
Viegas <mvrdgs@gmail.com>
2021-11-30 15:15:04 -0300

    funçao que soma numeros validos em uma string

--- mod32/dia_2/conteudo.py
+++ mod32/dia_2/conteudo.py
@@ -4,3 +4,16 @@ def vertical_print(word):
 
 
 vertical_print("palavra")
+
+
+def sum_numbers(numbers):
+    total = 0
+    for number in numbers.split(" "):
+        if str.isdigit(number):
+            total += int(number)
+        else:
+            print(f"Error: {number} is not a number")
+    print(f'Sum of valid values: {total}')
+
+
+sum_numbers("1 2 3 4 5 6 7 8 b")
