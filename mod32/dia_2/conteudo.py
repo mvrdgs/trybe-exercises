83955f4f6d9729f945eb3434a297a99b4461c644
Viegas <mvrdgs@gmail.com>
2021-11-30 16:07:59 -0300

    adicionado conteudo de leitura de arquivos

--- mod32/dia_2/conteudo.py
+++ mod32/dia_2/conteudo.py
@@ -15,7 +15,31 @@ def sum_numbers():
             total += int(number)
         else:
             print(f"Error: {number} is not a number")
-    print(f'Sum of valid values: {total}')
+    print(f"Sum of valid values: {total}")
 
 
 sum_numbers()
+
+file = open("arquivo.txt", mode="w")
+LINES = ["Olá\n", "mundo\n", "belo\n", "do\n", "Python\n"]
+file.writelines(LINES)
+file.close()
+
+# leitura
+file = open("arquivo.txt", mode="r")
+for line in file:
+    # não esqueça que a quebra de linha também é um caractere da linha
+    print(line)
+file.close()
+
+# escrita
+file = open("arquivo.dat", mode="wb")
+# o prefixo b em uma string indica que seu valor está codificado em bytes
+file.write(b"C\xc3\xa1ssio 30")
+file.close()
+
+# leitura
+file = open("arquivo.dat", mode="rb")
+content = file.read()
+print(content)  # saída: b'C\xc3\xa1ssio 30'
+file.close()  # não podemos esquecer de fechar o arquivo
