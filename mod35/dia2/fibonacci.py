9cb46c552bdb55ff8dcc5823e52f6e703a59c6c0
Viegas <mvrdgs@gmail.com>
2022-01-17 18:55:57 -0300

    fibonacci recursivo

--- mod35/dia2/fibonacci.py
+++ mod35/dia2/fibonacci.py
@@ -1,15 +1,22 @@
+# def fibonacci(n):
+#     first = 0
+#     second = 1
+#     result = [first, second]
+
+#     for i in range(n):
+#         sum = first + second
+#         result.append(first + second)
+#         first = second
+#         second = sum
+
+#     return result
+
+
 def fibonacci(n):
-    first = 0
-    second = 1
-    result = [first, second]
-
-    for i in range(n):
-        sum = first + second
-        result.append(first + second)
-        first = second
-        second = sum
-
-    return result
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 print(fibonacci(5))
